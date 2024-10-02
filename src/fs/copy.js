18e2254059666ff9649576772f8a440e3d692e07
Lisowez <lisowez@icloud.com>
2024-10-02 10:58:40 +0300

    feat: done fs/copy task

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,37 @@
+import * as fs from "fs/promises";
+import * as path from "path";
+import * as url from "url";
+
+const NEW_FOLDER_NAME = "files_copy";
+const OLD_FOLDER_NAME = "files";
+const TEXT_ERROR = "FS operation failed";
+
+const dirname = url.fileURLToPath(new URL(".", import.meta.url));
+
 const copy = async () => {
-    // Write your code here 
+  const pathForNewFolder = path.join(dirname, NEW_FOLDER_NAME);
+  const pathForOldFolder = path.join(dirname, OLD_FOLDER_NAME);
+  if (
+    (await fs.access(pathForNewFolder, fs.constants.F_OK).then(
+      () => true,
+      () => false
+    )) ||
+    !(await fs.access(pathForOldFolder, fs.constants.F_OK).then(
+      () => true,
+      () => false
+    ))
+  ) {
+    throw Error(TEXT_ERROR);
+  } else {
+    await fs.mkdir(pathForNewFolder);
+    const allFilesInOldFolder = await fs.readdir(pathForOldFolder);
+    allFilesInOldFolder.forEach((x) => {
+      fs.copyFile(
+        path.join(pathForOldFolder, x),
+        path.join(pathForNewFolder, x)
+      );
+    });
+  }
 };
 
 await copy();
