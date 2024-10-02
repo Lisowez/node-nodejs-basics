0ea4a3d30702bf75a5120cd579945775938f68a5
Lisowez <lisowez@icloud.com>
2024-10-02 17:28:53 +0300

    feat: done fs/read task

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,21 @@
+import * as fs from "fs/promises";
+import * as path from "path";
+import * as url from "url";
+import checkFile from "../utils/checkFile.js";
+
+const FILE_FOR_READ = "fileToRead.txt";
+const TEXT_ERROR = "FS operation failed";
+
+const dirname = url.fileURLToPath(new URL(".", import.meta.url));
+
 const read = async () => {
-    // Write your code here 
+  const pathFile = path.join(dirname, "files", FILE_FOR_READ);
+  if (await checkFile(pathFile)) {
+    const text = await fs.readFile(pathFile,{encoding:'utf-8'});
+    console.log(text);
+  } else {
+    throw Error(TEXT_ERROR);
+  }
 };
 
 await read();
