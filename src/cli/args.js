624e081af7b1f2ee7c4aa2debeee8550770ffa2d
Lisowez <lisowez@icloud.com>
2024-10-03 19:24:10 +0300

    feat: done cli/args task

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,14 @@
+const args = process.argv.slice(2);
+
 const parseArgs = () => {
-    // Write your code here 
+  const result = args.reduce(
+    (res, item, index) =>
+      index % 2
+        ? `${res}${item}${index === args.length - 1 ? "" : ", "}`
+        : `${res}${item.slice(2)} is `,
+    ""
+  );
+  console.log(result);
 };
 
 parseArgs();
