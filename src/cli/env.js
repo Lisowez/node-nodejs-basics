821a02e0654b108f9485cdd5335431e76e8f87e0
Lisowez <lisowez@icloud.com>
2024-10-03 19:10:58 +0300

    feat: done cli/env task

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,18 @@
+const envObject = process.env;
+const FILTER_WORD = "RSS_";
+
 const parseEnv = () => {
-    // Write your code here 
+  const keysEnvObject = Object.keys(envObject);
+  const filterKeys = keysEnvObject.filter((x) => x.startsWith(FILTER_WORD));
+
+  const result = filterKeys.reduce(
+    (res, item, index) =>
+      `${res}${item}=${envObject[item]}${
+        index === filterKeys.length - 1 ? "" : "; "
+      }`,
+    ""
+  );
+  console.log(result);
 };
 
 parseEnv();
