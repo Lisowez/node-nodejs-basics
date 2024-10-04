d2600d60b3655dbaa8a71a46c0cf1614274690eb
Lisowez <lisowez@icloud.com>
2024-10-04 13:56:57 +0300

    feat: done streams/read task

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,20 @@
+import { createReadStream } from "fs";
+import { stdout } from "process";
+import * as url from "url";
+import * as path from "path";
+
+const dirname = url.fileURLToPath(new URL(".", import.meta.url));
+const TARGET_FILE = "fileToRead.txt";
+
 const read = async () => {
-    // Write your code here 
+  const pathFile = path.join(dirname, "files", TARGET_FILE);
+  const stream = createReadStream(pathFile, "utf-8");
+  stream.on("data", (data) => {
+    stdout.write(data);
+  });
+  stream.on("error", (error) => {
+    throw error;
+  });
 };
 
 await read();
