923db96f4c30c32cc3b9f5c33732d1ccf4ef9cb1
Lisowez <lisowez@icloud.com>
2024-10-04 14:48:23 +0300

    feat: done zip/compress task

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,23 @@
+import * as url from "url";
+import * as path from "path";
+import * as zlib from "zlib";
+import { createWriteStream, createReadStream } from "fs";
+import { pipeline } from "stream";
+import { promisify } from "util";
+
+const dirname = url.fileURLToPath(new URL(".", import.meta.url));
+const TARGET_FILE = "fileToCompress.txt";
+const ARCHIVE_FILE = "archive.gz";
+
+const pipe = promisify(pipeline);
+
 const compress = async () => {
-    // Write your code here 
+  const pathTarget = path.join(dirname, "files", TARGET_FILE);
+  const pathArchive = path.join(dirname, "files", ARCHIVE_FILE);
+  const zlibActive = zlib.createGzip();
+  const readStream = createReadStream(pathTarget);
+  const writeStream = createWriteStream(pathArchive);
+  await pipe(readStream, zlibActive, writeStream);
 };
 
 await compress();
